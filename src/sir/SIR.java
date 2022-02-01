51130a03ca95c6aa7c04d93abba5760b9f1404b0
Mormi33 <mikamorillon@gmail.com>
2022-02-01 09:23:17 +0100

    Update SIR.java

--- src/sir/SIR.java
+++ src/sir/SIR.java
@@ -15,7 +15,7 @@ public class SIR {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hhh");
+        System.out.println("hola");
     }
     
 }
