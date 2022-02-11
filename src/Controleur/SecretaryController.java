237a7aa2bcb2bd8ea350cf9bc31d6216dbbc94bb
Mormi33 <mikamorillon@gmail.com>
2022-02-11 15:13:16 +0100

    SecretaryController

--- src/Controleur/SecretaryController.java
+++ src/Controleur/SecretaryController.java
@@ -6,12 +6,14 @@ package Controleur;
 
 import ConnexionBD.DatabaseAccessProperties;
 import Modele.Login;
+import java.sql.*;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 
 /**
  *
@@ -23,18 +25,6 @@ public class SecretaryController {
     
     public SecretaryController(Login user) {
         this.user = user;
-    }
-    
-    public void addPatient(String name, String surname, String adress, String gender, String ddn) {
-        //Vérifier infos
-        if (!name.equals("") && !surname.equals("") && !adress.equals("")) {
-            //Vérifier qu'un patient mêmes infos n'existe pas
-        
-            //Générer id
-            
-            
-            // transformer ddn en date
-        }   
     }
     
     public void recherchePatient(String critere, String recherche) {
@@ -49,40 +39,6 @@ public class SecretaryController {
         //recherchePatientByDdn(recherche);     
            
        
-    }
-        
-    
-    
-    public static void recherchePatientByCriteria(String critere, String recherche) throws SQLException {
-        
-        // Get a statement from the connection
-        Statement stmt = dap.getConn().createStatement() ;
-        
-        // Execute the query
-        ResultSet rsTest = stmt.executeQuery("SELECT * FROM PATIENT where " + critere + "= '" + recherche + "'") ;
-        while(rsTest.next()) { 
-            System.out.println(rsTest);
-            System.out.println("Id du patient : " + rsTest.getString(1));
-        }
-        // Close the result set, statement and the connection
-        rsTest.close() ;
-        stmt.close() ;
-    }
-
-    public static void recherchePatientByDdn(String critere, String recherche) throws SQLException {
-        
-        // Get a statement from the connection
-        Statement stmt = dap.getConn().createStatement() ;
-        
-        // Execute the query
-        ResultSet rsTest = stmt.executeQuery("SELECT * FROM PATIENT where birthDate = '" + recherche + "'") ;
-        while(rsTest.next()) { 
-            System.out.println(rsTest);
-            System.out.println("Id du patient : " + rsTest.getString(1));
-        }
-        // Close the result set, statement and the connection
-        rsTest.close() ;
-        stmt.close() ;
     }
     
 }
